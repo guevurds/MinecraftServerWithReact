8308b48682812106f5b2b70f0a73b53f1bc456db
guevurds <77858314+guevurds@users.noreply.github.com>
2022-01-18 01:43:43 -0300

    primeiro-update

---
+++ index.js
@@ -0,0 +1,48 @@
+const 
+    proc = require('child_process'),
+    util = require('util'),
+    express = require('express'),
+    app = express(),
+    http = require('http'),
+    server = http.createServer(app),
+    { Server } = require('socket.io'),
+    io = new Server(server);
+
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+      
+io.on('connection', (socket) => {
+    socket.on("start minecraft server", ()=> {
+        mc_server = proc.spawn(
+            "java",
+           ['-Xms512M', '-Xmx512M', '-jar', 'forge-1.12.2-14.23.5.2838-universal.jar', 'nogui'],
+           { cwd: "../rlserver2"}
+       )
+       
+       mc_server.stdout.on('data', function (data) {
+           if (data) {
+                socket.emit('logServer', ""+data);
+           }
+       });
+       
+       mc_server.stderr.on('data', function (data) {
+           if (data) {
+                socket.emit('logServer', ""+data);
+           }
+       });
+
+
+    })
+});
+   
+server.listen(80, () => {
+    console.log('listening on *:80');
+});
+
+
+    function startMinecraftServer(socket) {
+    
+
+}
